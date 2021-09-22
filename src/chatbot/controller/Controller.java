5b0829e7318e8aa7405be75e43744b8377c47bd1
jclegg03 <jjc2003@comcast.net>
2021-09-22 12:49:08 -0600

    Added the constructor, and the building blocks for the project

--- src/chatbot/controller/Controller.java
+++ src/chatbot/controller/Controller.java
@@ -1,11 +1,21 @@
 package chatbot.controller;
 
+import java.util.Scanner;
+import chatbot.model.Chatbot;
+
 public class Controller
 {
+	private Scanner input;
+	private Chatbot myChatbot;
 	
+	public Controller()
+	{
+		this.input = new Scanner(System.in);
+		this.myChatbot = new Chatbot("Yaj");
+	}
 	
 	public void start()
 	{
-		
+		System.out.println(myChatbot);
 	}
 }
--- src/chatbot/model/Chatbot.java
+++ src/chatbot/model/Chatbot.java
@@ -2,5 +2,30 @@ package chatbot.model;
 
 public class Chatbot
 {
-
+	private String name;
+	
+	public Chatbot(String name)
+	{
+		this.name = name;
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	public String processText(String text)
+	{
+		String answer = "you said: ";
+		answer += text;
+		return answer;
+	}
+	
+	@Override
+	public String toString()
+	{
+		String response = "Hi, I am a chatbot. ";
+		response += this.getName();
+		response += " is my name.";
+		return response;
+	}
 }
