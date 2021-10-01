ac1368f8b542d2f27b16ee4616bcf9d3214d965c
jclegg03 <jjc2003@comcast.net>
2021-10-01 13:26:29 -0600

    Implemented the popup windows

--- src/chatbot/controller/Controller.java
+++ src/chatbot/controller/Controller.java
@@ -2,29 +2,38 @@ package chatbot.controller;
 
 import java.util.Scanner;
 import chatbot.model.Chatbot;
+import chatbot.view.Popup;
 
 public class Controller
 {
 	private Scanner input;
 	private Chatbot myChatbot;
+	private Popup view;
 	
 	public Controller()
 	{
 		this.input = new Scanner(System.in);
 		this.myChatbot = new Chatbot("Yaj");
+		this.view = new Popup();
 	}
 	
 	public void start()
 	{
-		System.out.println(myChatbot);
-		System.out.println("What do you want to talk about?");
-		String response = input.nextLine();
+//		System.out.println(myChatbot);
+		view.displayMessage(myChatbot.toString());
+		
+//		System.out.println("What do you want to talk about?");
+//		String response = input.nextLine();
+		
+		String response = view.displayQuestion("What do you want to talk about?");
 		
 		while (! response.contentEquals("quit"))
 		{
-			System.out.println(interactWithChatbot(response));
-			System.out.println("Type quit to quit.");
-			response = input.nextLine();
+//			System.out.println(interactWithChatbot(response));
+			view.displayMessage(interactWithChatbot(response));
+//			System.out.println("Type quit to quit.");
+//			response = input.nextLine();
+			response = view.displayQuestion("Type quit to quit.");
 		}
 	}
 
